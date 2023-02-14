bfa23628ee9c1b5e171d4c827a9f03cd580e0a93
cedrata <81557163+cedrata@users.noreply.github.com>
2023-02-14 08:07:36 +0100

    Compiling git and main

---
+++ Tests/test.cpp
@@ -0,0 +1,22 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <memory>
+#include "branched.h"
+#include "branchless.h"
+
+
+TEST_CASE("BRANCHED VS BRANCLESS")
+{
+    // Variables instantiation.
+    std::unique_ptr<branched::FooBase> bched = std::make_unique<branched::FooDerivate>();
+    std::unique_ptr<branchless::FooBase> bless = std::make_unique<branchless::FooDerivate<branchless::Type1>>();
+
+    BENCHMARK("Branched code") {
+        return bched->process(true);
+    };
+
+    BENCHMARK("Branchless code") {
+        return bless->process();
+    };
+}
